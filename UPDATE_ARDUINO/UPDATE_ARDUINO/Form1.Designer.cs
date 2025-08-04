af2de789f798fa35d71a8d464d050a45e2c9aa52
Nguyen Minh Khoa <89793291+khoauuu123@users.noreply.github.com>
2025-08-04 10:25:29 +0700

    Add files via upload

---
+++ UPDATE_ARDUINO/UPDATE_ARDUINO/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace UPDATE_ARDUINO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tb_HexFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_terminal = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox_com = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // tb_HexFilePath
+            // 
+            this.tb_HexFilePath.Location = new System.Drawing.Point(11, 25);
+            this.tb_HexFilePath.Name = "tb_HexFilePath";
+            this.tb_HexFilePath.Size = new System.Drawing.Size(131, 20);
+            this.tb_HexFilePath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hex file";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "COM";
+            // 
+            // tb_terminal
+            // 
+            this.tb_terminal.Location = new System.Drawing.Point(199, 24);
+            this.tb_terminal.Multiline = true;
+            this.tb_terminal.Name = "tb_terminal";
+            this.tb_terminal.Size = new System.Drawing.Size(328, 97);
+            this.tb_terminal.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Com load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(106, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Upload";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox_com
+            // 
+            this.comboBox_com.FormattingEnabled = true;
+            this.comboBox_com.Location = new System.Drawing.Point(11, 71);
+            this.comboBox_com.Name = "comboBox_com";
+            this.comboBox_com.Size = new System.Drawing.Size(170, 21);
+            this.comboBox_com.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(538, 140);
+            this.Controls.Add(this.comboBox_com);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tb_terminal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_HexFilePath);
+            this.Name = "Form1";
+            this.Text = "update";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_HexFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_terminal;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox_com;
+    }
+}
+
---
+++ UPDATE_ARDUINO/UPDATE_ARDUINO/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Diagnostics;
+
+namespace UPDATE_ARDUINO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                comboBox_com.DataSource = SerialPort.GetPortNames();
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi trong MessageBox
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                comboBox_com.DataSource = SerialPort.GetPortNames();
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi trong MessageBox
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog
+                {
+                    Filter = "HEX Files (*.hex)|*.hex",
+                    Title = "Select HEX File"
+                };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    tb_HexFilePath.Text = openFileDialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi trong MessageBox
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string hexFilePath = tb_HexFilePath.Text;
+                string comPort = comboBox_com.Text;
+
+                if (string.IsNullOrEmpty(hexFilePath) || string.IsNullOrEmpty(comPort))
+                {
+                    MessageBox.Show("Please select a HEX file and COM port.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                DialogResult result = MessageBox.Show(
+                   "Bạn có chắc chắn muốn cập nhật firmware không?",
+                   "Xác nhận",
+                   MessageBoxButtons.YesNo,
+                   MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    // Call AVRDude
+                    UploadHexFile(hexFilePath, comPort);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi trong MessageBox
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void UploadHexFile(string hexFilePath, string comPort)
+        {
+
+            // Lấy đường dẫn tới file avrdude.exe và avrdude.conf trong thư mục dự án
+            string avrdudePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "avrdude");
+            //string avrdudePath = @"C:\Users\PC khoa\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/bin/avrdude";
+            string avrdudeConfPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "avrdude.conf");
+
+            // Arguments for avrdude
+            string  arguments = $"-C\"{avrdudeConfPath}\" -v -patmega2560 -cwiring -P{comPort} -b115200 -D -Uflash:w:\"{hexFilePath}\":i";
+
+
+            // Start process
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = avrdudePath,
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+
+            // Read output
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+
+            // Display output or error
+            if (process.ExitCode == 0)
+            {
+                tb_terminal.Text = "Upload successful: " + '\n';
+                tb_terminal.Text = tb_terminal.Text + output + error;
+            }
+            else
+            {
+                tb_terminal.Text = "Upload failed: " + '\n';
+                tb_terminal.Text = tb_terminal.Text + error;
+            }
+        }
+    }
+}
